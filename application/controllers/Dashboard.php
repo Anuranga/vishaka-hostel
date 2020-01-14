62b4a136612ffe61cb4d1c1e37cbf664a000440e
Jayan Pradeep Anuranga <jayan.anuranga@gmail.com>
2020-01-14 08:12:39 +0530

    get Count list

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -7,11 +7,33 @@ class Dashboard extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->helper('url');
+		$this->load->model('GatePassModel', 'gatepass');
+		$this->load->model('StudentsModel', 'student');
+		$this->load->model('VisitorsModel', 'visitor');
 	}
 
 	public function index()
 	{
-
-		$this->load->view('dashboard');
+		$data['gatePass'] = $this->gatePassCount();
+		$data['student'] = $this->GetStudentsCount();
+		$data['visitors'] = $this->GetVisitorsCount();
+		
+		$this->load->view('dashboard', $data);
 	}
+	
+	public function gatePassCount()
+	{
+		return $this->gatepass->getGatepassList();
+	}
+	
+	public function GetStudentsCount()
+	{
+		return $this->student->getStudentsCount();
+	}
+	
+	public function GetVisitorsCount()
+	{
+		return $this->visitors->getVisitorsCount();
+	}
+	
 }
